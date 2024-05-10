50ff0a1bc3d27d3606c4c00ab5ad0276375853a4
faco400 <vd.souza1999@gmail.com>
2024-05-10 19:31:42 -0300

    implemented task2

---
+++ task2.js
@@ -0,0 +1,34 @@
+function promiseAllSettled(arrayPromises) {
+  let results = []; // array to store values os resolved promises
+  let settledPromises = 0; // counter of settled Promises
+
+  return new Promise(function(myResolve) {
+    arrayPromises.forEach((element,index) => {
+      element.then(result =>{
+        results[index] = {status: 'fulfilled', value: result};
+      }).catch(error => {
+        results[index] = {status: 'fulfilled', value: error};
+      }).finally(() => {
+        settledPromises++;
+        if(settledPromises === arrayPromises.length)
+          myResolve(results);
+      });
+    });
+  });
+
+}
+
+
+const promises = [
+  Promise.resolve(1),
+  Promise.reject("Error occurred"),
+  Promise.resolve(3)
+];
+
+promiseAllSettled(promises)
+  .then(results => {
+    console.log("All promises settled:", results);
+    // Expected: [{ status: 'fulfilled', value: 1 },
+    //            { status: 'rejected', reason: 'Error occurred' },
+    //            { status: 'fulfilled', value: 3 }]
+  });
